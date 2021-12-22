94fccbd9fb3f6b77e9dc911b62e0d2ec00cb1659
Kiryl <kiryl.yushkevich@gmail.com>
2021-12-22 22:05:31 +0300

    fix: add comments for HumanReadableTime task

--- src/HumanReadableTime/index.ts
+++ src/HumanReadableTime/index.ts
@@ -1,27 +1,28 @@
+// custom Time class for storage data
 class Time {
+  // private fields
   private _hours: number;
   private _minutes: number;
   private _seconds: number;
 
-  public get hours(): number { return this._hours; }
-  public get minutes(): number { return this._minutes }
-  public get seconds(): number { return this._seconds }
+  constructor(inicialSeconds: number) {
+    // Convert seconds to hours, minutes and seconds
+    this._hours = Math.floor(inicialSeconds / 60 / 60);
+    this._minutes = Math.floor((inicialSeconds - this._hours * 60 * 60) / 60);
+    this._seconds = inicialSeconds - this._hours * 60 * 60 - this._minutes * 60;
+  }
 
-  public get getReadableTime(): string {
+  // Convert time to readable string
+  public getReadableTime(): string {
     const hoursString: string = this._hours < 10 ? `0${this._hours}` : `${this._hours}`;
     const minutesString: string = this._minutes < 10 ? `0${this._minutes}` : `${this._minutes}`;
     const secondsString: string = this._seconds < 10 ? `0${this._seconds}` : `${this._seconds}`;
     return `${hoursString}:${minutesString}:${secondsString}`;
   }
-
-  constructor(inicialSeconds: number) {
-    this._hours = Math.floor(inicialSeconds / 60 / 60);
-    this._minutes = Math.floor((inicialSeconds - this._hours * 60 * 60) / 60);
-    this._seconds = inicialSeconds - this._hours * 60 * 60 - this._minutes * 60;
-  }
 }
 
+// Execute functions 
 export function humanReadable(seconds:number):string {
   const time: Time = new Time(seconds);
-  return time.getReadableTime;
+  return time.getReadableTime();
 }
